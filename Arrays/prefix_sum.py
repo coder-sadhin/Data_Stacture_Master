8c6c9ab4aadf77f36627397f8d38c511fb5b8c04
coder-sadhin <codersadhin@gmail.com>
2024-08-06 18:59:38 +0600

    prefix

---
+++ Arrays/prefix_sum.py
@@ -0,0 +1,13 @@
+"""
+Author  : Alexander Pantyukhin
+Date    : November 3, 2022
+
+Implement the class of prefix sum with useful functions based on it.
+
+"""
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
