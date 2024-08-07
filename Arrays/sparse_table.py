6e475323582e625f17408f7ed05b834e57e63ffc
coder-sadhin <codersadhin@gmail.com>
2024-08-07 19:02:13 +0600

    sparse table error

--- Arrays/sparse_table.py
+++ Arrays/sparse_table.py
@@ -44,36 +44,18 @@ def build_sparse_table(number_list: list[int]) -> list[list[int]]:
         sparse_table[0][i] = value
     j = 1
 
-
-
-def query(sparse_table: list[list[int]], left_bound: int, right_bound: int) -> int:
-    """
-    >>> query(build_sparse_table([8, 1, 0, 3, 4, 9, 3]), 0, 4)
-    0
-    >>> query(build_sparse_table([8, 1, 0, 3, 4, 9, 3]), 4, 6)
-    3
-    >>> query(build_sparse_table([3, 1, 9]), 2, 2)
-    9
-    >>> query(build_sparse_table([3, 1, 9]), 0, 1)
-    1
-    >>> query(build_sparse_table([8, 1, 0, 3, 4, 9, 3]), 0, 11)
-    Traceback (most recent call last):
-    ...
-    IndexError: list index out of range
-    >>> query(build_sparse_table([]), 0, 0)
-    Traceback (most recent call last):
-    ...
-    ValueError: empty number list not allowed
-    """
-    if left_bound < 0 or right_bound >= len(sparse_table[0]):
-        raise IndexError("list index out of range")
-
-    # highest subset length of power of 2 that is within range [left_bound, right_bound]
-    j = int(log2(right_bound - left_bound + 1))
-
-    # minimum of 2 overlapping smaller subsets:
-    # [left_bound, left_bound + 2 ** j - 1] and [right_bound - 2 ** j + 1, right_bound]
-    return min(sparse_table[j][right_bound - (1 << j) + 1], sparse_table[j][left_bound])
+    # compute the minimum value for all intervals with size (2 ** j)
+    while (1 << j) <= length:
+        i = 0
+        # while subset starting from i still have at least (2 ** j) elements
+        while (i + (1 << j) - 1) < length:
+            # split range [i, i + 2 ** j] and find minimum of 2 halves
+            sparse_table[j][i] = min(
+                sparse_table[j - 1][i + (1 << (j - 1))], sparse_table[j - 1][i]
+            )
+            i += 1
+        j += 1
+    return sparse_table
 
 
 if __name__ == "__main__":
