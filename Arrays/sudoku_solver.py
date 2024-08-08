d1ff1f79fa091b3c31c412b02b0bceacd0552853
coder-sadhin <codersadhin@gmail.com>
2024-08-08 19:06:54 +0600

    sudoku solver corse

--- Arrays/sudoku_solver.py
+++ Arrays/sudoku_solver.py
@@ -8,6 +8,34 @@ import random
 import time
 
 
+def cross(items_a, items_b):
+    "Cross product of elements in A and elements in B."
+    return [a + b for a in items_a for b in items_b]
+
+
+digits = "123456789"
+rows = "ABCDEFGHI"
+cols = digits
+squares = cross(rows, cols)
+unitlist = (
+    [cross(rows, c) for c in cols]
+    + [cross(r, cols) for r in rows]
+    + [cross(rs, cs) for rs in ("ABC", "DEF", "GHI") for cs in ("123", "456", "789")]
+)
+units = {s: [u for u in unitlist if s in u] for s in squares}
+peers = {s: set(sum(units[s], [])) - {s} for s in squares}  # noqa: RUF017
+
+
+
+grid1 = (
+    "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
+)
+grid2 = (
+    "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
+)
+hard1 = (
+    ".....6....59.....82....8....45........3........6..3.54...325..6.................."
+)
 
 if __name__ == "__main__":
     test()
