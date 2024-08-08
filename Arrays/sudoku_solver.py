76bae25dd97d922397c5bd1d76d32c944dbd7c30
coder-sadhin <codersadhin@gmail.com>
2024-08-08 19:05:48 +0600

    sudoku solver

---
+++ Arrays/sudoku_solver.py
@@ -0,0 +1,5 @@
+"""
+Please do not modify this file!  It is published at https://norvig.com/sudoku.html with
+only minimal changes to work with modern versions of Python.  If you have improvements,
+please make them in a separate file.
+"""
