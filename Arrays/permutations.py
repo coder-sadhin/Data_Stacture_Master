3930402130b18718da93ee195b805f24151f2ffa
coder-sadhin <codersadhin@gmail.com>
2024-08-10 19:18:09 +0600

    permute_backtrack complete and solved the issue

--- Arrays/permutations.py
+++ Arrays/permutations.py
@@ -26,7 +26,19 @@ def permute_backtrack(nums: list[int]) -> list[list[int]]:
     [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]
     """
 
-    
+    def backtrack(start: int) -> None:
+        if start == len(nums) - 1:
+            output.append(nums[:])
+        else:
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]  # backtrack
+
+    output: list[list[int]] = []
+    backtrack(0)
+    return output
+
 
 if __name__ == "__main__":
     import doctest
