eaa8718ac0fbbd28167676ff93aeaa8984b528da
coder-sadhin <codersadhin@gmail.com>
2024-08-10 19:17:06 +0600

    permutetion recarsive

--- Arrays/permutations.py
+++ Arrays/permutations.py
@@ -1,4 +1,11 @@
+def permute_recursive(nums: list[int]) -> list[list[int]]:
+    """
+    Return all permutations.
 
+    >>> permute_recursive([1, 2, 3])
+    [[3, 2, 1], [2, 3, 1], [1, 3, 2], [3, 1, 2], [2, 1, 3], [1, 2, 3]]
+    """
+    
 
 if __name__ == "__main__":
     import doctest
