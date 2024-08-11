6bf6bce5ecde18eb2cbde235ca2758cdc65a256f
coder-sadhin <codersadhin@gmail.com>
2024-08-11 19:19:24 +0600

    kth lergest element initial

--- Arrays/kth_largest_element.py
+++ Arrays/kth_largest_element.py
@@ -3,3 +3,11 @@ Given an array of integers and an integer k, find the kth largest element in the
 
 https://stackoverflow.com/questions/251781
 """
+
+
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
