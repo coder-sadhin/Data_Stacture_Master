efa932ed17a0444f3bfab8b3b89402a8c69721b5
coder-sadhin <codersadhin@gmail.com>
2024-08-12 19:23:41 +0600

    monotonic array initial

--- Arrays/monotonic_array.py
+++ Arrays/monotonic_array.py
@@ -1,1 +1,9 @@
 # https://leetcode.com/problems/monotonic-array/
+
+
+# Test the function with your examples
+if __name__ == "__main__":
+    # Test the function with your examples
+    print(is_monotonic([1, 2, 2, 3]))  # Output: True
+    print(is_monotonic([6, 5, 4, 4]))  # Output: True
+    print(is_monotonic([1, 3, 2]))  # Output: False
