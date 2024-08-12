23cddf5105d46c326790f65a73718ea187d96957
coder-sadhin <codersadhin@gmail.com>
2024-08-12 19:23:59 +0600

    add class

--- Arrays/monotonic_array.py
+++ Arrays/monotonic_array.py
@@ -1,4 +1,16 @@
 # https://leetcode.com/problems/monotonic-array/
+def is_monotonic(nums: list[int]) -> bool:
+    """
+    Check if a list is monotonic.
+
+    >>> is_monotonic([1, 2, 2, 3])
+    True
+    >>> is_monotonic([6, 5, 4, 4])
+    True
+    >>> is_monotonic([1, 3, 2])
+    False
+    """
+   
 
 
 # Test the function with your examples
