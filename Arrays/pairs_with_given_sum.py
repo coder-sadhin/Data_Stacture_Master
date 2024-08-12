7a0d8351c75aa5008f3e30b3ffcd52e489ba95b6
coder-sadhin <codersadhin@gmail.com>
2024-08-12 19:27:04 +0600

    iter tools

--- Arrays/pairs_with_given_sum.py
+++ Arrays/pairs_with_given_sum.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
+"""
+Given an array of integers and an integer req_sum, find the number of pairs of array
+elements whose sum is equal to req_sum.
 
+https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/0
+"""
+
+from itertools import combinations
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
