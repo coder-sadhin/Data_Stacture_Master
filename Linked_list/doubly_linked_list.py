9cee2db0dc1d6c5c3a9d4eaa2c890953083c0bea
coder-sadhin <codersadhin@gmail.com>
2024-08-13 19:35:42 +0600

    doubly linked list class

--- Linked_list/doubly_linked_list.py
+++ Linked_list/doubly_linked_list.py
@@ -8,6 +8,26 @@ class Node:
         return f"{self.data}"
 
 
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        """
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.insert_at_head('b')
+        >>> linked_list.insert_at_head('a')
+        >>> linked_list.insert_at_tail('c')
+        >>> tuple(linked_list)
+        ('a', 'b', 'c')
+        """
+        node = self.head
+        while node:
+            yield node.data
+            node = node.next
+
+
 if __name__ == "__main__":
     from doctest import testmod
 
