2aa38d2a779e729ce02dfd78577c3c3006a5dea5
coder-sadhin <codersadhin@gmail.com>
2024-08-13 19:37:45 +0600

    insert n th position

--- Linked_list/doubly_linked_list.py
+++ Linked_list/doubly_linked_list.py
@@ -54,6 +54,52 @@ class DoublyLinkedList:
     def insert_at_tail(self, data):
         self.insert_at_nth(len(self), data)
 
+    def insert_at_nth(self, index: int, data):
+        """
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.insert_at_nth(-1, 666)
+        Traceback (most recent call last):
+            ....
+        IndexError: list index out of range
+        >>> linked_list.insert_at_nth(1, 666)
+        Traceback (most recent call last):
+            ....
+        IndexError: list index out of range
+        >>> linked_list.insert_at_nth(0, 2)
+        >>> linked_list.insert_at_nth(0, 1)
+        >>> linked_list.insert_at_nth(2, 4)
+        >>> linked_list.insert_at_nth(2, 3)
+        >>> str(linked_list)
+        '1->2->3->4'
+        >>> linked_list.insert_at_nth(5, 5)
+        Traceback (most recent call last):
+            ....
+        IndexError: list index out of range
+        """
+        length = len(self)
+
+        if not 0 <= index <= length:
+            raise IndexError("list index out of range")
+        new_node = Node(data)
+        if self.head is None:
+            self.head = self.tail = new_node
+        elif index == 0:
+            self.head.previous = new_node
+            new_node.next = self.head
+            self.head = new_node
+        elif index == length:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+            self.tail = new_node
+        else:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            temp.previous.next = new_node
+            new_node.previous = temp.previous
+            new_node.next = temp
+            temp.previous = new_node
+
 
 if __name__ == "__main__":
     from doctest import testmod
