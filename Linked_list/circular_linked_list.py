dc0431b88f7164bea210703e972a7d4175f12b67
coder-sadhin <codersadhin@gmail.com>
2024-08-15 19:46:32 +0600

    create_linked_lis

--- Linked_list/circular_linked_list.py
+++ Linked_list/circular_linked_list.py
@@ -5,6 +5,12 @@ from dataclasses import dataclass
 from typing import Any
 
 
+@dataclass
+class Node:
+    data: Any
+    next_node: Node | None = None
+
+
 if __name__ == "__main__":
     import doctest
 
