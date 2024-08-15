09e4df29e79d8a3a59705398c024f292c774555b
coder-sadhin <codersadhin@gmail.com>
2024-08-15 19:46:51 +0600

    lenth

--- Linked_list/circular_linked_list.py
+++ Linked_list/circular_linked_list.py
@@ -11,6 +11,24 @@ class Node:
     next_node: Node | None = None
 
 
+@dataclass
+class CircularLinkedList:
+    head: Node | None = None  # Reference to the head (first node)
+    tail: Node | None = None  # Reference to the tail (last node)
+
+    def __iter__(self) -> Iterator[Any]:
+        """
+        Iterate through all nodes in the Circular Linked List yielding their data.
+        Yields:
+            The data of each node in the linked list.
+        """
+        node = self.head
+        while node:
+            yield node.data
+            node = node.next_node
+            if node == self.head:
+                break
+
 if __name__ == "__main__":
     import doctest
 
