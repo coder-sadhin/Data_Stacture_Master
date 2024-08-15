85a9aacba1d9d3320c21d9c2548df13d7021b579
coder-sadhin <codersadhin@gmail.com>
2024-08-15 19:46:19 +0600

    circular linked list initializetion and data class

---
+++ Linked_list/circular_linked_list.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from collections.abc import Iterator
+from dataclasses import dataclass
+from typing import Any
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
